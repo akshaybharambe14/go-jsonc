37810bbb92e345e89d75da3e7ef0573176d27a3a
Akshay Bharambe <akshaybharambe14@gmail.com>
2020-05-07 22:32:36 +0530

    FIx: Slow decode by reducing comparisions

--- jsonc.go
+++ jsonc.go
@@ -1,3 +1,11 @@
+// package jsonc provides APIs to read commented (.jsonc) files.
+// It implements io.Reader which makes it compatible with standard library.
+
+// EX.
+// 		...
+// 		ba, _ := ioutil.Readall(jsonc.NewDecoder(r)) // r is io.Reader
+// 		...
+
 package jsonc
 
 import (
@@ -8,19 +16,27 @@ import (
 )
 
 type (
-	state int
-
-	comment struct {
-		state   state
-		multiLn bool
-		isJSON  bool
-	}
-
-	// Decoder implements io.Reader. It wraps provided source.
+	// Decoder represents a .jsonc decoder. It implements io.Reader.
 	Decoder struct {
 		r io.Reader
 		c comment
 	}
+
+	// comment represents status of Decoder while reading json document.
+	comment struct {
+		state   state
+		multiLn bool // if true, a multiline comment
+		isJSON  bool // if true, decoder is processing either a "key" or "value"
+	}
+
+	state int
+)
+
+const (
+	stopped  state = iota // 0, default state, started with 0 to make `zero value` of `comment` useful.
+	canStart              // 1
+	started               // 2
+	canStop               // 3
 )
 
 const (
@@ -35,18 +51,81 @@ const (
 	charN    = 110 // (n)
 )
 
-const (
-	stopped  state = iota // 0, default state
-	canStart              // 1
-	started               // 2
-	canStop               // 3
-)
-
 var (
 	ErrUnexpectedEndOfComment = errors.New("unexpected end of comment")
 )
 
-// NewDecoder returns a new Decoder wrapping the provided io.Reader. The returned decoder implements io.Reader.
+func (c *comment) reset() {
+	c.state = stopped
+	c.multiLn = false
+}
+
+func (c *comment) complete() bool {
+	return c.state == stopped
+}
+
+func decode(p []byte, c *comment) int {
+	i := 0
+	for _, s := range p {
+
+		switch c.state {
+
+		case stopped:
+			if s == quote { // all characters between "" are valid, can be added to result
+				c.isJSON = !c.isJSON
+			}
+
+			if c.isJSON {
+				p[i] = s
+				i++
+				continue
+			}
+
+			// TODO: formatting can be preserved by allowing space and tab in JSON
+			if s == space || s == tab || s == newLine {
+				continue
+			}
+
+			if s == fwdSlash {
+				c.state = canStart
+				continue
+			}
+
+			p[i] = s
+			i++
+
+		case canStart:
+
+			if s == star || s == fwdSlash {
+				c.state = started
+			}
+
+			c.multiLn = (s == star)
+
+		case started:
+
+			if s == star || s == bkdSlash {
+				c.state = canStop
+			}
+
+			if s == newLine && !c.multiLn {
+				c.reset()
+			}
+
+		case canStop:
+
+			if s == fwdSlash || s == charN {
+				c.reset()
+			}
+
+		}
+
+	}
+
+	return i
+}
+
+// NewDecoder returns a new Decoder wrapping the provided io.Reader.
 func NewDecoder(r io.Reader) *Decoder {
 	return &Decoder{
 		c: comment{},
@@ -57,7 +136,7 @@ func NewDecoder(r io.Reader) *Decoder {
 // Read reads from underlying reader and processes the stream to omit comments.
 // A single read doesn't guaranttee a valid JSON. Depends on length of passed slice.
 //
-// Produces ErrUnexpectedEndOfComment for incomplete comments.
+// Produces ErrUnexpectedEndOfComment for incomplete comments else if not nil, then it is an error from underlying reader.
 func (d *Decoder) Read(p []byte) (int, error) {
 
 	n, err := d.r.Read(p)
@@ -65,7 +144,7 @@ func (d *Decoder) Read(p []byte) (int, error) {
 		return 0, err
 	}
 
-	shortRead := n <= len(p)
+	shortRead := n <= len(p) // data to be processed is less than length of `p`
 	n = decode(p[:n], &d.c)
 
 	if shortRead && !d.c.complete() {
@@ -73,80 +152,6 @@ func (d *Decoder) Read(p []byte) (int, error) {
 	}
 
 	return n, nil
-}
-
-func decode(p []byte, c *comment) int {
-	i := 0
-	for _, s := range p {
-		if c.handle(s) {
-			p[i] = s
-			i++
-		}
-	}
-
-	return i
-}
-
-// handle the current byte, if returned true, add the byte to result.
-func (c *comment) handle(s byte) bool {
-	switch c.state {
-
-	case stopped:
-		if s == quote { // all characters between "" are valid, can be added to result
-			c.isJSON = !c.isJSON
-		}
-
-		if c.isJSON {
-			return true
-		}
-
-		if s == space || s == tab || s == newLine {
-			return false
-		}
-
-		if s == fwdSlash {
-			c.state = canStart
-			return false
-		}
-
-		return true
-
-	case canStart:
-
-		if s == star || s == fwdSlash {
-			c.state = started
-		}
-
-		c.multiLn = (s == star)
-
-	case started:
-
-		if s == star || s == bkdSlash {
-			c.state = canStop
-		}
-
-		if s == newLine && !c.multiLn {
-			c.reset()
-		}
-
-	case canStop:
-
-		if s == fwdSlash || s == charN {
-			c.reset()
-		}
-
-	}
-
-	return false
-}
-
-func (c *comment) reset() {
-	c.state = stopped
-	c.multiLn = false
-}
-
-func (c *comment) complete() bool {
-	return c.state == stopped
 }
 
 // DecodeBytes decodes passed commented json byte slice to normal json.
